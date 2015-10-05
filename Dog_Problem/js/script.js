1c708e9db0a50893baf0c0e1757ea51bfe733e69
Jarrod Smith <jasmith2@fullsail.edu>
2015-10-04 21:21:35 -0500

    Dog problem output
    This is the output for the dog problem.

--- Dog_Problem/js/script.js
+++ Dog_Problem/js/script.js
@@ -15,6 +15,8 @@ var rateAge= 7;         //Rate of dog aging
 
 //Exprssions
 
-var dogYears = dogHAge * rateAge;
+var dogYears = dogHAge * rateAge;   //This is the calculation to figure from human to dog years.
 
-//Outputs 
+//Outputs
+
+console.log ("If Spikes age is " + dogHAge + " in human years. Then he is " + dogYears + " In dog years.");
