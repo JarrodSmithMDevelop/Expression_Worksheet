46cd61178cb4596e79a25ab52ddf025551038bf2
Jarrod Smith <jasmith2@fullsail.edu>
2015-10-04 21:10:18 -0500

    Start to Dog Problem
    This is the start to the dog problem on the worksheet.

---
+++ Dog_Problem/js/script.js
@@ -0,0 +1,20 @@
+/**
+ * Created by JarrodSmith on 10/4/15.
+ */
+
+//Jarrod Smith
+//SDI Section 1
+//Dog Problem
+
+
+// Variables
+
+var dogHAge= 5;         //Dogs age in human years
+var dogYears= 0;        //Dogs age in dog years
+var rateAge= 7;         //Rate of dog aging
+
+//Exprssions
+
+var dogYears = dogHAge * rateAge;
+
+//Outputs 
