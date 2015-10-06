96b2384ee663a1a8f6dd361e1fe5e556574a5ec0
Jarrod Smith <jasmith2@fullsail.edu>
2015-10-05 21:33:21 -0500

    Slice of pie part 1 complete
    Slice of pie part 1 is complete and ready for review.

--- Slice of pie/js/script.js
+++ Slice of pie/js/script.js
@@ -1,3 +1,22 @@
 /**
- * Created by JarrodSmith on 10/4/15.
+ * Jarrod Smith
+ * SDI 1510 Section 1
+ * Slice of pie part 1.
+ * 10-05-15
  */
+
+
+
+var slicesPer = 10;         //Amount of slices per pizza
+var numPeople = 14;         //Amount of people who are attending the party.
+var numPizzas = 10;         //The total amount of pizzas ordered.
+var totalSlices;            //Total amount of slices of pizza.
+
+
+//Equations
+
+var totalSlices = slicesPer * numPizzas;         //Equating total number of slices of pizza ordered.
+
+//Output
+
+console.log("Each person ate " + totalSlices / numPeople + " number slices of pizza.");      //Output the result to console.
