8c6fd805e2d895e439bc5942d8cf2584529685fc
Jarrod Smith <jasmith2@fullsail.edu>
2015-10-07 21:39:44 -0500

    complete slice of pie part 2
    Work out the equations to figure how many slice as a whole number and
    the remainder.

--- Slice of pie Part 2/js/pie2script.js
+++ Slice of pie Part 2/js/pie2script.js
@@ -10,14 +10,16 @@ var slicesPer = 10;         //Amount of slices per pizza
 var numPeople = 14;         //Amount of people who are attending the party.
 var numPizzas = 10;         //The total amount of pizzas ordered.
 var totalSlices;            //Total amount of slices of pizza.
-var slicesPer;              //Total number of slices per person
+var spikesPizza;            //Total number of slices spike gets
+var slicesPerPerson;        //Total number of slices per person
 
 //Equations
 
 var totalSlices = slicesPer * numPizzas;         //Equating total number of slices of pizza ordered.
-
-
+var spikesPizza = totalSlices % numPeople;       //The amount of slices that spike will get.
+var slicesPerPerson = parseInt(totalSlices / numPeople);
 
 //Output
 
-console.log("Each person ate " + totalSlices / numPeople + " number slices of pizza.");      //Output the result to console.
+console.log("Each person ate " + slicesPerPerson + " number slices of pizza.");      //Output the result to console.
+console.log("The amount of pizza spike will get " + spikesPizza + ".");              //Output to show how many slices spike gets.
